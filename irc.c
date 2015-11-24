d884667a0c9639f74164bb32b15e2d5152216f79
snekez <mrcat575@yahoo.co.uk>
2015-11-24 20:48:14 +0200

    Update irc.c

--- irc.c
+++ irc.c
@@ -1,5 +1,5 @@
-/* Alright so far this guy answers to the ".snake", ".snoke", ".snare" and ".mad" commands. 
-                                      Is also pretty dumb */
+/* Available commands are:
+".snake", ".snoke", ".snare", ".mad" and ".holy" */
 
 
 #include "socket.h"
